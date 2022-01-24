c4e0c90f21103d1692594d60d5018308adc0eacc
928280 <Varsha.Hemnani@Cognizant.com>
2022-01-24 10:50:29 +0100

    Add Search component and mdb bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { SearchComponent } from './search/search.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductDetailComponent,
+    SearchComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    BrowserAnimationsModule,
+    MDBBootstrapModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
